823faeaab1fb9d4a923b492a86c3b975d7d2faac
David Shahmatov <david.shahmatov.work@gmail.com>
2026-07-06 14:18:58 +0300

    fix/fix_format_date

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,23 +1,9 @@
+import { format } from "date-fns";
+
 export const formatDate = (isoString: string) => {
-  const d = new Date(isoString);
-  const local = new Date(d.getTime() - d.getTimezoneOffset() * 60000);
-
-  const year = local.getFullYear();
-  const month = String(local.getMonth() + 1).padStart(2, "0");
-  const day = String(local.getDate()).padStart(2, "0");
-
-  const hours = String(local.getHours()).padStart(2, "0");
-  const minutes = String(local.getMinutes()).padStart(2, "0");
-
-  return `${year}.${month}.${day} , ${hours}:${minutes}`;
+  return format(new Date(isoString), "yyyy.MM.dd , HH:mm");
 };
 
 export const formatTime = (isoString: string) => {
-  const date = new Date(isoString);
-
-  return date.toLocaleTimeString([], {
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: false,
-  });
+  return format(new Date(isoString), "HH:mm");
 };
