fb63303cb04940d4fe0cd8d4d92a826f07338a88
David Shahmatov <david.shahmatov.work@gmail.com>
2026-07-16 15:37:11 +0300

    fix/change_screen_height_to_dvh

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -25,7 +25,7 @@ export const MainLayout = ({ children }: DashboardLayoutProps) => {
           </div>
           <main
             className={clsx(
-              "flex-1 h-full flex flex-col scrollbar-hidden max-1024px:pl-0 [@media(min-width:1024px)_and_(max-width:1339px)]:pl-[250px] min-2000px:pl-[20vw] pl-[315px] overflow-auto min-h-screen min-2000px:pt-[3.5vw]",
+              "flex-1 h-full flex flex-col scrollbar-hidden max-1024px:pl-0 [@media(min-width:1024px)_and_(max-width:1339px)]:pl-[250px] min-2000px:pl-[20vw] pl-[315px] overflow-auto min-h-dvh min-2000px:pt-[3.5vw]",
               !location.pathname.includes("/chats") &&
                 "max-1024px:!px-[16px] [@media(min-width:1024px)_and_(max-width:1339px)]:pr-[0px] max-768px:pt-[80px] min-2000px:pt-[6vw] pt-[116px] max-768px:pb-10 pb-[100px] min-2000px:pr-[7vw] pr-[100px]",
             )}
